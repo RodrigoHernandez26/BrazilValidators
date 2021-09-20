d0987268f30042e906b18f50c80939e79b1df2f6
RodrigoHernandez26 <rodrigohsouza26@gmail.com>
2021-09-20 14:57:41 -0300

    added cnpj validator

--- brasil.ts
+++ brasil.ts
@@ -14,7 +14,7 @@ export class BrazilValidator {
                 let numbers, digits, sum, i, result, equalDigits;
                 equalDigits = 1;
                 if (cpf.length < 11) {
-                    return null;
+                    return { cpfNotValid: true };
                 }
 
                 for (i = 0; i < cpf.length - 1; i++) {
@@ -53,6 +53,63 @@ export class BrazilValidator {
                     return { cpfNotValid: true };
                 }
             }
+            return null;
+        };
+    }
+
+    static isValidCNPJ() {
+        return (control: AbstractControl) => {
+            var cnpj = control.value;
+            if (cnpj) {
+                var tamanho, numeros, digitos, soma, pos, i, resultado
+                cnpj = cnpj.replace(/[^\d]+/g,'');
+            
+                if(cnpj == '') return { cnpjNotValid: true };
+                if (cnpj.length != 14) return { cnpjNotValid: true };
+            
+                // Elimina CNPJs invalidos conhecidos
+                if (cnpj == "00000000000000" || 
+                    cnpj == "11111111111111" || 
+                    cnpj == "22222222222222" || 
+                    cnpj == "33333333333333" || 
+                    cnpj == "44444444444444" || 
+                    cnpj == "55555555555555" || 
+                    cnpj == "66666666666666" || 
+                    cnpj == "77777777777777" || 
+                    cnpj == "88888888888888" || 
+                    cnpj == "99999999999999")
+                    return { cnpjNotValid: true };
+                    
+                // Valida DVs
+                tamanho = cnpj.length - 2
+                numeros = cnpj.substring(0,tamanho);
+                digitos = cnpj.substring(tamanho);
+                soma = 0;
+                pos = tamanho - 7;
+                for (i = tamanho; i >= 1; i--) {
+                    soma += numeros.charAt(tamanho - i) * pos--;
+                    if (pos < 2)
+                        pos = 9;
+                    }
+                resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+                if (resultado != digitos.charAt(0))
+                    return { cnpjNotValid: true };
+                    
+                tamanho = tamanho + 1;
+                numeros = cnpj.substring(0,tamanho);
+                soma = 0;
+                pos = tamanho - 7;
+                for (i = tamanho; i >= 1; i--) {
+                    soma += numeros.charAt(tamanho - i) * pos--;
+                    if (pos < 2)
+                        pos = 9;
+                    };
+                resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+                if (resultado != digitos.charAt(1))
+                    return { cnpjNotValid: true };
+                    
+                return null;
+            };
             return null;
         };
     }
